2ef270fe6c8cdb80ad5d8d1ef06f22534671e249
MrTomMus <tommusshow@gmail.com>
2024-09-01 00:43:18 +0300

    feat: Реализована первоначальная разметка экрана

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
                 <div className={`${styles.container} ${styles['header-block']}`}>
                     {<div className={styles.logo}><p>Github Search</p></div> /* TODO заменить на ссылку перенаправляющую на главную страницу */}
                     <nav className={styles.nav}>
-                        <ul className={styles['nav-list']}>
+                        <ul className={styles['nav-list']}> {/* TODO реализовать роутинг */}
                             <li>Поиск</li>
                             <li>Избранные</li>
                         </ul>
